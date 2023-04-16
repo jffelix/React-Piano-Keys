ca5f37435d6dcb2aee15ab4ca81ed64d96965788
Jean-Luc Felix <jlfelix522@yahoo.com>
2023-04-16 12:33:51 -0700

    Added Gb and G Notes

--- my-app/src/keyboard/Keyboard.js
+++ my-app/src/keyboard/Keyboard.js
@@ -5,33 +5,14 @@ import DNote from "../assets/notes_D.mp3";
 import EbNote from "../assets/notes_Eb.mp3";
 import ENote from "../assets/notes_E.mp3";
 import FNote from "../assets/notes_F.mp3";
-
+import GbNote from "../assets/notes_Gb.mp3";
+import GNote from "../assets/notes_G.mp3";
 import ANote from "../assets/notes_A.mp3";
 import AbNote from "../assets/notes_Ab.mp3";
 import BNote from "../assets/notes_B.mp3";
 import BbNote from "../assets/notes_Bb.mp3";
 
 function Keyboard() {
-
-    function playAbNote() {
-        new Audio(AbNote).play();
-        console.log("You clicked Ab Note!");
-    }
-
-    function playANote() {
-        new Audio(ANote).play();
-        console.log("You clicked A Note!");
-    }
-
-    function playBbNote() {
-        new Audio(BbNote).play();
-        console.log("You clicked Bb Note!");
-    }
-
-    function playBNote() {
-        new Audio(BNote).play();
-        console.log("You clicked B Note!");
-    }
 
     function playCNote() {
         new Audio(CNote).play();
@@ -63,6 +44,36 @@ function Keyboard() {
         console.log("You clicked F Note!");
     }
 
+    function playGbNote() {
+        new Audio(GbNote).play();
+        console.log("You clicked Gb Note!");
+    }
+
+    function playGNote() {
+        new Audio(GNote).play();
+        console.log("You clicked G Note!");
+    }
+
+    function playAbNote() {
+        new Audio(AbNote).play();
+        console.log("You clicked Ab Note!");
+    }
+
+    function playANote() {
+        new Audio(ANote).play();
+        console.log("You clicked A Note!");
+    }
+
+    function playBbNote() {
+        new Audio(BbNote).play();
+        console.log("You clicked Bb Note!");
+    }
+
+    function playBNote() {
+        new Audio(BNote).play();
+        console.log("You clicked B Note!");
+    }
+
 
     return (
         <div className ="keyboard">
@@ -84,7 +95,12 @@ function Keyboard() {
             <div className="naturalNotes">
                 <button onClick={playFNote}>F</button>
             </div>
-
+            <div className="sharpFlatNotes">
+                <button onClick={playGbNote}>Gb</button>
+            </div>
+            <div className="naturalNotes">
+                <button onClick={playGNote}>G</button>
+            </div>
             <div className="sharpFlatNotes">
                 <button onClick={playAbNote}>Ab</button>
             </div>
