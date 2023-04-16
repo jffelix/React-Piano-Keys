685d418c64c57c57762cb73c0cf582f3aebfbd1b
Jean-Luc Felix <jlfelix522@yahoo.com>
2023-04-16 12:14:19 -0700

    Added B and Bb Notes

--- my-app/src/keyboard/Keyboard.js
+++ my-app/src/keyboard/Keyboard.js
@@ -1,21 +1,46 @@
 import './Keyboard.css';
 import ANote from "../assets/notes_A.mp3";
+import AbNote from "../assets/notes_Ab.mp3";
+import BNote from "../assets/notes_B.mp3";
+import BbNote from "../assets/notes_Bb.mp3";
 
 function Keyboard() {
 
+    function playAbNote() {
+        new Audio(AbNote).play();
+        console.log("You clicked Ab Note!");
+    }
+
     function playANote() {
         new Audio(ANote).play();
-        console.log("You clicked playNoteTest!");
+        console.log("You clicked A Note!");
     }
+
+    function playBbNote() {
+        new Audio(BbNote).play();
+        console.log("You clicked Bb Note!");
+    }
+
+    function playBNote() {
+        new Audio(BNote).play();
+        console.log("You clicked B Note!");
+    }
+
 
     return (
         <div>
             <h2>Hello from Keyboard Component!</h2>
+            <div className="sharpFlatNotes">
+                <button onClick={playAbNote}>Ab</button>
+            </div>
             <div className="naturalNotes">
                 <button onClick={playANote}>A</button>
             </div>
             <div className="sharpFlatNotes">
-                <button>Ab</button>
+                <button onClick={playBbNote}>Bb</button>
+            </div>
+            <div className="naturalNotes">
+                <button onClick={playBNote}>B</button>
             </div>
         </div>
     )
