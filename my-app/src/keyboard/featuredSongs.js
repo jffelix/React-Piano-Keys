9596b752c0991737ecc8b8f737913c80d96ac278
Jean-Luc Felix <jlfelix522@yahoo.com>
2023-04-17 21:24:53 -0700

    Implemented shelter

--- my-app/src/keyboard/featuredSongs.js
+++ my-app/src/keyboard/featuredSongs.js
@@ -1,6 +1,6 @@
 import "./featuredSongs.css";
 
-function featuredSongs(props)  {
+function FeaturedSongs(props)  {
 
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -59,11 +59,101 @@ function featuredSongs(props)  {
         props.toggleKeyColorE();
     }
 
+    async function shelter() {
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(750);
+        new Audio(props.ANote).play();
+        props.toggleKeyColorA();
+        await sleep(250);
+        new Audio(props.GNote).play();
+        props.toggleKeyColorG();
+        await sleep(500);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(1000);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(750);
+        new Audio(props.ANote).play();
+        props.toggleKeyColorA();
+        await sleep(250);
+        new Audio(props.GNote).play();
+        props.toggleKeyColorG();
+        await sleep(500);
+        new Audio(props.DNote).play();
+        props.toggleKeyColorD();
+        await sleep(750);
+        // 8 count
+        new Audio(props.DNote).play();
+        props.toggleKeyColorD();
+        await sleep(250);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(500);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(750);
+        new Audio(props.ANote).play();
+        props.toggleKeyColorA();
+        await sleep(250);
+        new Audio(props.GNote).play();
+        props.toggleKeyColorG();
+        await sleep(500);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(1000);
+        // 8 count
+        new Audio(props.ENote).play();
+        props.toggleKeyColorE();
+        await sleep(500);
+        new Audio(props.ENote).play();
+        props.toggleKeyColorE();
+        await sleep(250);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(250);
+        new Audio(props.DNote).play();
+        props.toggleKeyColorD();
+        await sleep(250);
+        new Audio(props.ENote).play();
+        props.toggleKeyColorE();
+        await sleep(250);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(250);
+        new Audio(props.DNote).play();
+        props.toggleKeyColorD();
+        await sleep(250);
+        new Audio(props.ENote).play();
+        props.toggleKeyColorE();
+        await sleep(250);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(250);
+        new Audio(props.DNote).play();
+        props.toggleKeyColorD();
+        await sleep(250);
+        new Audio(props.ENote).play();
+        props.toggleKeyColorE();
+        await sleep(250);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(250);
+        new Audio(props.DNote).play();
+        props.toggleKeyColorD();
+        await sleep(250);
+        new Audio(props.CNote).play();
+        props.toggleKeyColorC();
+        await sleep(1000);
+    }
+
     return (
-        <div className="iAintWorried">
+        <div className="featuredSongs">
             <button onClick={iAintWorried}>I Ain't Worried</button>
+            <button onClick={shelter}>Shelter</button>
         </div>
     )
 }
 
-export default featuredSongs;
+export default FeaturedSongs;
