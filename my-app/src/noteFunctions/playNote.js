a8a76290e154b83fc7e736189e7f5f801693b285
Jean-Luc Felix <jlfelix522@yahoo.com>
2023-12-27 20:03:22 -0800

    Commented out console.logs in playNote

--- my-app/src/noteFunctions/playNote.js
+++ my-app/src/noteFunctions/playNote.js
@@ -13,61 +13,61 @@ import BbNote from "../assets/notes_Bb.mp3";
 
 function playCNote() {
     new Audio(CNote).play();
-    console.log("You clicked C Note!");
+    // console.log("You clicked C Note!");
 }
 
 function playDbNote() {
     new Audio(DbNote).play();
-    console.log("You clicked Db Note!");
+    // console.log("You clicked Db Note!");
 }
 function playDNote() {
     new Audio(DNote).play();
-    console.log("You clicked D Note!");
+    // console.log("You clicked D Note!");
 }
 
 function playEbNote() {
     new Audio(EbNote).play();
-    console.log("You clicked Eb Note!");
+    // console.log("You clicked Eb Note!");
 }
 
 function playENote() {
     new Audio(ENote).play();
-    console.log("You clicked E Note!");
+    // console.log("You clicked E Note!");
 }
 
 function playFNote() {
     new Audio(FNote).play();
-    console.log("You clicked F Note!");
+    // console.log("You clicked F Note!");
 }
 
 function playGbNote() {
     new Audio(GbNote).play();
-    console.log("You clicked Gb Note!");
+    // console.log("You clicked Gb Note!");
 }
 
 function playGNote() {
     new Audio(GNote).play();
-    console.log("You clicked G Note!");
+    // console.log("You clicked G Note!");
 }
 
 function playAbNote() {
     new Audio(AbNote).play();
-    console.log("You clicked Ab Note!");
+    // console.log("You clicked Ab Note!");
 }
 
 function playANote() {
     new Audio(ANote).play();
-    console.log("You clicked A Note!");
+    // console.log("You clicked A Note!");
 }
 
 function playBbNote() {
     new Audio(BbNote).play();
-    console.log("You clicked Bb Note!");
+    // console.log("You clicked Bb Note!");
 }
 
 function playBNote() {
     new Audio(BNote).play();
-    console.log("You clicked B Note!");
+    // console.log("You clicked B Note!");
 }
 
 export default {
